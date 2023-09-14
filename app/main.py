0570b31390223894785cd27c3faae081cbbdcabe
Sukesh Reddy Rondla <65500920+SukeshRondla@users.noreply.github.com>
2023-09-14 17:16:06 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,17 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from . import models
 from .database import engine
 from .routers import post, user, auth, vote
 from .config import settings
 
-
-# models.Base.metadata.create_all(bind=engine)
-
 app = FastAPI()
 
-origins = ["*"]
+# Load CORS origins from environment variables or configure it as needed.
+origins = ["*"]  # You can replace this with settings.CORS_ORIGINS or an environment variable.
 
 app.add_middleware(
     CORSMiddleware,
@@ -25,7 +22,6 @@ app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
-
 
 @app.get("/")
 def root():
