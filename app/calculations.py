5d63ed2113ab521f23459a63b075da9ef1d1a677
Sukesh Reddy Rondla <65500920+SukeshRondla@users.noreply.github.com>
2023-09-26 17:45:10 -0500

    Update calculations.py

--- app/calculations.py
+++ app/calculations.py
@@ -1,25 +1,7 @@
-def add(num1: int, num2: 2):
-    return num1 + num2
-
-
-def subtract(num1: int, num2: int):
-    return num1 - num2
-
-
-def multiply(num1: int, num2: int):
-    return num1 * num2
-
-
-def divide(num1: int, num2: int):
-
-    return num1 / num2
-
-
 class InsufficientFunds(Exception):
     pass
 
-
-class BankAccount():
+class BankAccount:
     def __init__(self, starting_balance=0):
         self.balance = starting_balance
 
@@ -29,8 +11,21 @@ class BankAccount():
     def withdraw(self, amount):
         if amount > self.balance:
             raise InsufficientFunds("Insufficient funds in account")
-
         self.balance -= amount
 
     def collect_interest(self):
         self.balance *= 1.1
+
+def add(num1: int, num2: int) -> int:
+    return num1 + num2
+
+def subtract(num1: int, num2: int) -> int:
+    return num1 - num2
+
+def multiply(num1: int, num2: int) -> int:
+    return num1 * num2
+
+def divide(num1: int, num2: int) -> float:
+    if num2 == 0:
+        raise ValueError("Cannot divide by zero")
+    return num1 / num2
